4267631866f4bd2c78c59a61c9a79ab7cde08cd4
Rafael-Duarte13 <jrd.neto3047@gmail.com>
2018-08-24 15:00:44 -0300

    primeiro commit

---
+++ visualizar-assembleia.php
@@ -0,0 +1,105 @@
+<?php
+require_once(__DIR__ . "/classes/modelo/Assembleia.class.php");
+require_once(__DIR__ . "/classes/dao/AssembleiaDAO.class.php");
+?>
+<?php
+$assembleia = new Assembleia();
+$dao = new AssembleiaDAO();
+$assembleias = $dao->findAll();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/all.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/login.css">
+	<link rel="stylesheet" href="assets/css/home.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/base.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/botoes.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+</head>
+<body>
+    <!-- Menu lateral -->
+    <div class="sidenav">
+        <li>
+            <a href="index.php"><i class="fa fa-home"></i> <span>Home</span></a>
+        </li>  
+        
+        <button class="dropdown-btn"><i class="fa fa-bars"></i> <span>Assembléia</span>  
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-container">
+                <button class="dropdown-btn"><i class="fa fa-bars"></i> <span>Cadastrar</span>  
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-container">
+                    <a href="#">Assembleia</a>
+                    <a href="#">Pauta</a>
+                    <a href="#">Tipo de Assembleia</a>
+                </div>
+                <a href="visualizar-assembleia.php">Visualizar</a>
+        </div>
+
+        <button class="dropdown-btn"><i class="fa fa-users"></i> <span>Morador</span>  
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-container">
+            <a href="#">Cadastrar</a>
+            <a href="#">Visualizar</a>
+        </div>
+    </div>
+        
+    <div class="main">
+    <!-- <h2>Sidebar Dropdown</h2>
+    <p>Click on the dropdown button to open the dropdown menu inside the side navigation.</p>
+    <p>This sidebar is of full height (100%) and always shown.</p>
+    <p>Some random text..</p> -->
+    </div>
+        
+    <script>
+        /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+        var dropdown = document.getElementsByClassName("dropdown-btn");
+        var i;
+
+        for (i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var dropdownContent = this.nextElementSibling;
+                if (dropdownContent.style.display === "block") {
+                    dropdownContent.style.display = "none";
+                } else {
+                    dropdownContent.style.display = "block";
+                }
+            });
+        }
+    </script>
+    <!-- Fim menu lateral -->
+
+    <div class="container">
+        <table class="table table-sm table-hover mt-5">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">assembleia</th>
+                    <th scope="col">data</th>
+                    <th scope="col">ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($assembleias as $assembleia): ?>
+                    <tr>
+                        <th scope="col"><?=$assembleia->getId();?></th>
+                        <td><?=$assembleia->getNome();?></td>
+                        <td><?=$assembleia->getData();?></td>
+                        <td>
+                            <button type="submit"><i class="far fa-edit"></i></button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html> 
